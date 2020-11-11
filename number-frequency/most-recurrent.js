d331ee0cf993d346cba71d0f96731410d211d9f8
Shnaeem <62766356+Shnaeem@users.noreply.github.com>
2020-11-10 19:21:25 -0500

    Update most-recurrent.js

--- number-frequency/most-recurrent.js
+++ number-frequency/most-recurrent.js
@@ -1,5 +1,13 @@
-const mostRecurrent = (set) => {
-  return set
-}
+// [1, 2, 3, 4, 3], your algorithm should return 3
 
-module.exports = mostRecurrent
+
+const mostRecurrent = (set) => {
+  return set.sort((a, b) =>
+    set.filter(v => v === a).length - set.filter(v => v === b).length
+  ).pop();
+};
+
+
+
+
+module.exports = mostRecurrent;
